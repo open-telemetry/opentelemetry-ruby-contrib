37696bd2c5c8c58c072937e69834b562bf2c42a5
Ran Nozik <ran@gethelios.dev>
2022-07-02 06:05:29 +0300

    feat: add request/response hooks to more http clients (#71)
    * add http instrumentation hooks
    
    * add http client instrumentation hooks
    
    * add rest client instrumentation hooks
    
    * faraday support
    
    * pr comments

--- instrumentation/http/lib/opentelemetry/instrumentation/http/patches/client.rb
+++ instrumentation/http/lib/opentelemetry/instrumentation/http/patches/client.rb
@@ -25,13 +25,25 @@ module OpenTelemetry
 
             tracer.in_span("HTTP #{request_method}", attributes: attributes, kind: :client) do |span|
               OpenTelemetry.propagation.inject(req.headers)
+              safe_execute_hook(instrumentation_config[:request_hook], span, req) unless instrumentation_config[:request_hook].nil?
               super.tap do |response|
                 annotate_span_with_response!(span, response)
+                safe_execute_hook(instrumentation_config[:response_hook], span, response) unless instrumentation_config[:response_hook].nil?
               end
             end
           end
 
           private
+
+          def instrumentation_config
+            HTTP::Instrumentation.instance.config
+          end
+
+          def safe_execute_hook(hook, *args)
+            hook.call(*args)
+          rescue StandardError => e
+            OpenTelemetry.handle_error(exception: e)
+          end
 
           def annotate_span_with_response!(span, response)
             return unless response&.status
