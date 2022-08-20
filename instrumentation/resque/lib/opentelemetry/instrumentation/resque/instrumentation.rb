11e1521a1389dd5f395fe3600391c489333f436c
Nicholas Volker <45795784+nvolker@users.noreply.github.com>
2022-08-20 18:50:38 +0200

    Add a configuration explainer

--- instrumentation/resque/lib/opentelemetry/instrumentation/resque/instrumentation.rb
+++ instrumentation/resque/lib/opentelemetry/instrumentation/resque/instrumentation.rb
@@ -18,6 +18,27 @@ module OpenTelemetry
           defined?(::Resque)
         end
 
+        ## Supported configuration keys for the install config hash:
+        #
+        # force_flush: when `true`, all completed spans will be synchronously flushed
+        #   at the end of a job's execution (default: `false`). You will likely wish to
+        #   enable this option for job systems that fork worker processes such as Resque.
+        #
+        # span_naming: when `:job_class`, the span names will be set to
+        #   '<job class name> <operation>'. When `:queue`, the span names
+        #   will be set to '<destination / queue name> <operation>'
+        #
+        # propagation_style: controls how the job's execution is traced and related
+        #   to the trace where the job was enqueued. Can be one of:
+        #
+        #   - :link (default) - the job will be executed in a separate trace. The
+        #     initial span of the execution trace will be linked to the span that
+        #     enqueued the job, via a Span Link.
+        #   - :child - the job will be executed in the same logical trace, as a direct
+        #     child of the span that enqueued the job.
+        #   - :none - the job's execution will not be explicitly linked to the span that
+        #     enqueued the job.
+
         option :force_flush,       default: false,  validate: :boolean
         option :span_naming,       default: :queue, validate: %I[job_class queue]
         option :propagation_style, default: :link,  validate: %i[link child none]
