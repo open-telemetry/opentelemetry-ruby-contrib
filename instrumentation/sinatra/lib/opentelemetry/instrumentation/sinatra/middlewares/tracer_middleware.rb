d4d404c0d43de66e02fd0fd74192b6722e5b7476
Sander Jans <scbjans@gmail.com>
2023-02-11 21:15:45 +0100

    fix: add exceptions to sinatra spans, ruboproof test. (#328)

--- instrumentation/sinatra/lib/opentelemetry/instrumentation/sinatra/middlewares/tracer_middleware.rb
+++ instrumentation/sinatra/lib/opentelemetry/instrumentation/sinatra/middlewares/tracer_middleware.rb
@@ -16,17 +16,25 @@ module OpenTelemetry
           end
 
           def call(env)
-            @app.call(env)
+            response = @app.call(env)
           ensure
-            trace_response(env)
+            trace_response(env, response)
           end
 
-          def trace_response(env)
+          def trace_response(env, response)
             span = OpenTelemetry::Instrumentation::Rack.current_span
             return unless span.recording?
 
             span.set_attribute('http.route', env['sinatra.route'].split.last) if env['sinatra.route']
             span.name = env['sinatra.route'] if env['sinatra.route']
+
+            return if response.nil?
+
+            sinatra_response = ::Sinatra::Response.new([], response.first)
+            return unless sinatra_response.server_error?
+
+            span.record_exception(env['sinatra.error']) if env['sinatra.error']
+            span.status = OpenTelemetry::Trace::Status.error
           end
         end
       end
