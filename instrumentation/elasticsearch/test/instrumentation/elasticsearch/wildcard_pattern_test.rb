6159b325819408854a63dd9c0e5b55f01e09e41b
Emily Stolfo <emily.stolfo@elastic.co>
2023-03-02 13:04:22 +0100

    feat: Add Wildcard pattern tests

---
+++ instrumentation/elasticsearch/test/instrumentation/elasticsearch/wildcard_pattern_test.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# Copyright The OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'test_helper'
+
+require_relative '../../../lib/opentelemetry/instrumentation/elasticsearch'
+
+describe OpenTelemetry::Instrumentation::Elasticsearch::WildcardPattern do
+  let(:wildcard_obj) { OpenTelemetry::Instrumentation::Elasticsearch::WildcardPattern.new(pattern) }
+  JSON.parse(
+    File.read("test/fixtures/wildcard_matcher_tests.json", :encoding => 'utf-8')
+  ).each do |category, group|
+    describe(category) do
+      group.each do |pattern_from_json, examples|
+        let(:pattern) { pattern_from_json }
+
+        examples.each do |string, expectation|
+          it("#{pattern_from_json} #{expectation ? '=~' : '!~'} #{string}") do
+            _(wildcard_obj.match?(string)).must_equal(expectation)
+          end
+        end
+      end
+    end
+  end
+end
