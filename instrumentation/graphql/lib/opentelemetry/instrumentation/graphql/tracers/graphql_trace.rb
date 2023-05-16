384513ebce633367fc32f24d9718448b235039c0
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2023-05-16 11:06:04 -0400

    Refactor away from using PlatformTrace
    The base functionality of `PlatformTrace` is re-implemented to only use
    what we need (eg: platform key caches). Each trace method is defined
    explicitly without metaprogramming. The new kwargs are used to avoid
    extra hash allocations.

--- instrumentation/graphql/lib/opentelemetry/instrumentation/graphql/tracers/graphql_trace.rb
+++ instrumentation/graphql/lib/opentelemetry/instrumentation/graphql/tracers/graphql_trace.rb
@@ -13,116 +13,145 @@ module OpenTelemetry
         # GraphQLTrace contains the OpenTelemetry tracer implementation compatible with
         # the new GraphQL tracing API (>= 2.0.18)
         module GraphQLTrace # rubocop:disable Metrics/ModuleLength
-          def self.platform_keys
-            {
-              'lex' => 'graphql.lex',
-              'parse' => 'graphql.parse',
-              'validate' => 'graphql.validate',
-              'analyze_query' => 'graphql.analyze_query',
-              'analyze_multiplex' => 'graphql.analyze_multiplex',
-              'execute_query' => 'graphql.execute_query',
-              'execute_query_lazy' => 'graphql.execute_query_lazy',
-              'execute_multiplex' => 'graphql.execute_multiplex'
-            }
+          def initialize(trace_scalars: false, **_options)
+            @trace_scalars = trace_scalars
+            @platform_field_key_cache = Hash.new { |h, k| h[k] = platform_field_key(k) }
+            @platform_authorized_key_cache = Hash.new { |h, k| h[k] = platform_authorized_key(k) }
+            @platform_resolve_type_key_cache = Hash.new { |h, k| h[k] = platform_resolve_type_key(k) }
+            super
           end
 
-          platform_keys.each do |trace_method, platform_key|
-            module_eval(
-              #  def lex(**data, &block)
-              #    attributes = attributes_for("lex", data)
-              #
-              #    tracer.in_span("graphql.lex", attributes: attributes) do |span|
-              #      super.tap do |response|
-              #        errors = response[:errors]&.compact&.map(&:to_h)&.to_json if "lex" == 'validate'
-              #        unless errors.nil?
-              #          span.add_event(
-              #            'graphql.validation.error',
-              #            attributes: {
-              #              'message' => errors
-              #            }
-              #          )
-              #        end
-              #      end
-              #    end
-              #  end
-              <<-RUBY, __FILE__, __LINE__ + 1
-                def #{trace_method}(**data, &block)
-                  attributes = attributes_for("#{trace_method}", data)
+          def execute_multiplex(multiplex:, &block)
+            tracer.in_span('graphql.execute_multiplex', &block)
+          end
 
-                  tracer.in_span("#{platform_key}", attributes: attributes) do |span|
-                    super.tap do |response|
-                      errors = response[:errors]&.compact&.map(&:to_h)&.to_json if "#{trace_method}" == 'validate'
-                      unless errors.nil?
-                        span.add_event(
-                          'graphql.validation.error',
-                          attributes: {
-                            'message' => errors
-                          }
-                        )
-                      end
-                    end
-                  end
+          def lex(query_string:, &block)
+            tracer.in_span('graphql.lex', &block)
+          end
+
+          def parse(query_string:, &block)
+            tracer.in_span('graphql.parse', &block)
+          end
+
+          def validate(query:, validate:, &block)
+            tracer.in_span('graphql.validate') do |span|
+              super.tap do |response|
+                errors = response[:errors]&.compact&.map(&:to_h)&.to_json
+                unless errors.nil?
+                  span.add_event(
+                    'graphql.validation.error',
+                    attributes: {
+                      'message' => errors
+                    }
+                  )
                 end
-              RUBY
-            )
+              end
+            end
           end
 
-          %i[execute_field execute_field_lazy].each do |trace_method|
-            module_eval(
-              # def platform_execute_field(platform_key, data, &block)
-              #   instrument_execution(platform_key, "execute_field", data, &block)
-              # end
-              <<-RUBY, __FILE__, __LINE__ + 1
-                def platform_#{trace_method}(platform_key, data, &block)
-                  instrument_execution(platform_key, "#{trace_method}", data, &block)
-                end
-              RUBY
-            )
+          def analyze_multiplex(multiplex:, &block)
+            tracer.in_span('graphql.analyze_query', &block)
           end
 
-          %i[authorized authorized_lazy].each do |trace_method|
-            module_eval(
-              #  def authorized(type:, query:, **_rest)
-              #    platform_key = @platform_authorized_key_cache[type]
-              #    return super unless platform_key
-
-              #    instrument_execution(platform_key, "authorized", { query: query, type: type }) do
-              #      super
-              #    end
-              #  end
-              <<-RUBY, __FILE__, __LINE__ + 1
-                def #{trace_method}(type:, query:, **_rest)
-                  platform_key = @platform_authorized_key_cache[type]
-                  return super unless platform_key
-
-                  instrument_execution(platform_key, "#{trace_method}", { query: query, type: type }) do
-                    super
-                  end
-                end
-              RUBY
-            )
+          def analyze_query(query:, &block)
+            tracer.in_span('graphql.analyze_multiplex', &block)
           end
 
-          %i[resolve_type resolve_type_lazy].each do |trace_method|
-            module_eval(
-              # def resolve_type(type:, query:, **_rest)
-              #   platform_key = @platform_resolve_type_key_cache[type]
-              #   instrument_execution(platform_key, "resolve_type", { query: query, type: type }) do
-              #     super
-              #   end
-              # end
-              <<-RUBY, __FILE__, __LINE__ + 1
-                def #{trace_method}(type:, query:, **_rest)
-                  platform_key = @platform_resolve_type_key_cache[type]
-                  instrument_execution(platform_key, "#{trace_method}", { query: query, type: type }) do
-                    super
-                  end
-                end
-              RUBY
-            )
+          def execute_query(query:, &block)
+            attributes = {}
+            attributes['graphql.operation.name'] = query.selected_operation_name if query.selected_operation_name
+            attributes['graphql.operation.type'] = query.selected_operation.operation_type
+            attributes['graphql.document'] = query.query_string
+
+            tracer.in_span('graphql.execute_query', attributes: attributes, &block)
           end
 
-          include ::GraphQL::Tracing::PlatformTrace
+          def execute_query_lazy(query:, &block)
+            tracer.in_span('graphql.execute_query_lazy', &block)
+          end
+
+          def execute_field(field:, query:, ast_node:, arguments:, object:, &block)
+            platform_key = platform_execute_field_key(field)
+            return super unless platform_key
+
+            attributes = {}
+            attributes['graphql.field.parent'] = field.owner&.graphql_name
+            attributes['graphql.field.name'] = field.graphql_name
+            attributes['graphql.field.lazy'] = false
+
+            tracer.in_span(platform_key, attributes: attributes, &block)
+          end
+
+          def execute_field_lazy(field:, query:, ast_node:, arguments:, object:, &block)
+            platform_key = platform_execute_field_key(field)
+            return super unless platform_key
+
+            attributes = {}
+            attributes['graphql.field.parent'] = field.owner&.graphql_name
+            attributes['graphql.field.name'] = field.graphql_name
+            attributes['graphql.field.lazy'] = true
+
+            tracer.in_span(platform_key, attributes: attributes, &block)
+          end
+
+          def authorized(query:, type:, object:, &block)
+            platform_key = @platform_authorized_key_cache[type]
+            return super unless platform_key
+
+            attributes = {}
+            attributes['graphql.type.name'] = type.graphql_name
+            attributes['graphql.lazy'] = false
+
+            tracer.in_span(platform_key, attributes: attributes, &block)
+          end
+
+          def authorized_lazy(query:, type:, object:, &block)
+            platform_key = @platform_authorized_key_cache[type]
+            return super unless platform_key
+
+            attributes = {}
+            attributes['graphql.type.name'] = type.graphql_name
+            attributes['graphql.lazy'] = true
+
+            tracer.in_span(platform_key, attributes: attributes, &block)
+          end
+
+          def resolve_type(query:, type:, object:)
+            platform_key = @platform_resolve_type_key_cache[type]
+
+            attributes = {}
+            attributes['graphql.type.name'] = type.graphql_name
+            attributes['graphql.lazy'] = false
+
+            tracer.in_span(platform_key, attributes: attributes, &block)
+          end
+
+          def resolve_type_lazy(query:, type:, object:)
+            platform_key = @platform_resolve_type_key_cache[type]
+
+            attributes = {}
+            attributes['graphql.type.name'] = type.graphql_name
+            attributes['graphql.lazy'] = true
+
+            tracer.in_span(platform_key, attributes: attributes, &block)
+          end
+
+          private
+
+          def platform_execute_field_key(field:, &block)
+            platform_key = @platform_field_key_cache[field] if trace_field?(field)
+            platform_key if !platform_key.nil? && trace_field
+          end
+
+          def trace_field?(field)
+            return_type = field.type.unwrap
+
+            if return_type.kind.scalar? || return_type.kind.enum?
+              (field.trace.nil? && @trace_scalars) || field.trace
+            else
+              true
+            end
+          end
 
           def platform_field_key(field)
             return unless config[:enable_platform_field]
@@ -154,40 +183,12 @@ module OpenTelemetry
             end
           end
 
-          private
-
-          def instrument_execution(platform_key, key, data, &block)
-            attributes = attributes_for(key, data)
-            tracer.in_span(platform_key, attributes: attributes, &block)
-          end
-
           def tracer
             GraphQL::Instrumentation.instance.tracer
           end
 
           def config
             GraphQL::Instrumentation.instance.config
-          end
-
-          def attributes_for(key, data) # rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
-            attributes = {}
-            case key
-            when 'execute_field', 'execute_field_lazy'
-              attributes['graphql.field.parent'] = data[:field]&.owner&.graphql_name
-              attributes['graphql.field.name'] = data[:field]&.graphql_name
-              attributes['graphql.lazy'] = key == 'execute_field_lazy'
-            when 'authorized', 'authorized_lazy'
-              attributes['graphql.type.name'] = data[:type]&.graphql_name
-              attributes['graphql.lazy'] = key == 'authorized_lazy'
-            when 'resolve_type', 'resolve_type_lazy'
-              attributes['graphql.type.name'] = data[:type]&.graphql_name
-              attributes['graphql.lazy'] = key == 'resolve_type_lazy'
-            when 'execute_query'
-              attributes['graphql.operation.name'] = data[:query].selected_operation_name if data[:query].selected_operation_name
-              attributes['graphql.operation.type'] = data[:query].selected_operation.operation_type
-              attributes['graphql.document'] = data[:query].query_string
-            end
-            attributes
           end
         end
       end
