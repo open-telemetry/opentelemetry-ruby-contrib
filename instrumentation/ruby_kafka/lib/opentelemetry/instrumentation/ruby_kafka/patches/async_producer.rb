e8032f5d513f16a3c39ae61c44bc1621c10e8010
Sam Handler <sam.handler@shopify.com>
2023-07-17 15:43:37 -0600

    propagate in separate method

--- instrumentation/ruby_kafka/lib/opentelemetry/instrumentation/ruby_kafka/patches/async_producer.rb
+++ instrumentation/ruby_kafka/lib/opentelemetry/instrumentation/ruby_kafka/patches/async_producer.rb
@@ -7,12 +7,17 @@ module OpenTelemetry
         # The AsyncProducer module contains the instrumentation patch the AsyncProducer#produce method
         module AsyncProducer
           def produce(value, topic:, **options)
+            options = __otel_merge_options!(**options)
+            super
+          end
+
+          def __otel_merge_options!(**options)
             options ||= { headers: {} }
             # The propagator mutates the carrier (first positional argument), so we need to set headers to empty hash so
             # that there's something to mutate
             options[:headers] = {} unless options[:headers]
             OpenTelemetry.propagation.inject(options[:headers])
-            super
+            options
           end
         end
       end
