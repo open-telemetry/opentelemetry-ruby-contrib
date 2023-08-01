6daf41011c28e6baef0a20a8aa7b4f8d172bc5e1
Robert Laurin <robert.laurin@shopify.com>
2023-08-01 09:17:18 -0500

    fix: test name contraction

--- instrumentation/graphql/test/instrumentation/graphql/instrumentation_test.rb
+++ instrumentation/graphql/test/instrumentation/graphql/instrumentation_test.rb
@@ -73,7 +73,7 @@ describe OpenTelemetry::Instrumentation::GraphQL do
         _(SomeGraphQLAppSchema.tracers[0].class).must_equal(expected_tracer)
       end
 
-      it 'does not install instrumentation on gem versions that don't support it' do
+      it 'does not install instrumentation on gem versions that do not support it' do
         skip if instrumentation.supports_legacy_tracer?
 
         instrumentation.install(config)
