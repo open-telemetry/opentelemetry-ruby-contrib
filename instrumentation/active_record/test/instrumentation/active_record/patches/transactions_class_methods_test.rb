3625d5f479b3bb5b124897ee80053a4f84f55650
Matt <alpmatthew@gmail.com>
2023-08-14 11:34:51 -0400

    fix: ensure that transaction name property is used, rather than self (#617)
    This ensures that if a class is dynamically instantiated (singleton class / metaclass) such as by the Blazer gem, the transaction name string is used for the code namespace rather than setting the span name to something like #<Class:0x00007fc635f69cf0>(FooBar)

--- instrumentation/active_record/test/instrumentation/active_record/patches/transactions_class_methods_test.rb
+++ instrumentation/active_record/test/instrumentation/active_record/patches/transactions_class_methods_test.rb
@@ -19,14 +19,33 @@ describe OpenTelemetry::Instrumentation::ActiveRecord::Patches::TransactionsClas
     it 'traces' do
       User.transaction { User.create! }
 
-      transaction_span = spans.find { |s| s.name == 'User.transaction' }
+      transaction_span = spans.find { |s| s.attributes['code.namespace'] == 'User' }
       _(transaction_span).wont_be_nil
     end
 
     it 'traces base transactions' do
       ActiveRecord::Base.transaction { User.create! }
 
-      transaction_span = spans.find { |s| s.name == 'ActiveRecord::Base.transaction' }
+      transaction_span = spans.find { |s| s.name == 'ActiveRecord.transaction' }
+      _(transaction_span).wont_be_nil
+    end
+
+    it 'traces dynamically created transaction classes' do
+      klass = Class.new(User) do
+        def self.name
+          'Klass'
+        end
+      end
+      klass.transaction { klass.create! }
+
+      transaction_span = spans.find { |s| s.attributes['code.namespace'] == 'Klass' }
+      _(transaction_span).wont_be_nil
+    end
+
+    it 'records transaction name as code namespace' do
+      ActiveRecord::Base.transaction { User.create! }
+
+      transaction_span = spans.find { |s| s.attributes['code.namespace'] == 'ActiveRecord::Base' }
       _(transaction_span).wont_be_nil
     end
   end
