3d0e3b3b43af76dcc37d873b7eea4c304f3f0b7a
Ariel Valentin <arielvalentin@users.noreply.github.com>
2023-09-02 14:12:22 +0000

    refactor: inline method

--- instrumentation/active_support/lib/opentelemetry/instrumentation/active_support/span_subscriber.rb
+++ instrumentation/active_support/lib/opentelemetry/instrumentation/active_support/span_subscriber.rb
@@ -102,7 +102,7 @@ module OpenTelemetry
           token = payload.delete(:__opentelemetry_ctx_token)
           return unless span && token
 
-          payload = transform_payload(payload)
+          payload = handler.transform_payload(payload)
           attrs = payload.map do |k, v|
             [k.to_s, sanitized_value(v)] if valid_payload_key?(k) && valid_payload_value?(v)
           end
@@ -118,10 +118,6 @@ module OpenTelemetry
         end
 
         private
-
-        def transform_payload(payload)
-          handler.transform_payload(payload)
-        end
 
         def valid_payload_key?(key)
           handler.valid_payload_key?(key)
