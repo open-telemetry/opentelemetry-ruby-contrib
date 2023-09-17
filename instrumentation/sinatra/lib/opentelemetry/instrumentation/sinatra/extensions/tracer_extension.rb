eb7e7978e82f5be4449a94cfedf57a389b9b29d6
Jurgens du Toit <jrgns@jrgns.net>
2023-09-17 10:48:47 +0200

    feat: Trace route_eval in Sinatra

--- instrumentation/sinatra/lib/opentelemetry/instrumentation/sinatra/extensions/tracer_extension.rb
+++ instrumentation/sinatra/lib/opentelemetry/instrumentation/sinatra/extensions/tracer_extension.rb
@@ -13,10 +13,29 @@ module OpenTelemetry
         # Sinatra extension that installs TracerMiddleware and provides
         # tracing for template rendering
         module TracerExtension
+
           # Sinatra hook after extension is registered
           def self.registered(app)
-            # Create tracing `render` method
+
             ::Sinatra::Base.module_eval do
+              # Invoked when a matching route is found.
+              # This method yields directly to user code.
+              def route_eval
+                Sinatra::Instrumentation.instance.tracer.in_span(
+                  'sinatra.route_eval',
+                  attributes: {
+                    'sinatra.route' => request.path,
+                    'sinatra.request_method' => request.request_method,
+                    'sinatra.resource' => "#{request.request_method} #{request.path}",
+                  }
+                ) do
+                  throw :halt, yield
+                  # Can't use super?
+                  # super
+                end
+              end
+
+              # Create tracing `render` method
               def render(_engine, data, *)
                 template_name = data.is_a?(Symbol) ? data : :literal
 
@@ -28,6 +47,7 @@ module OpenTelemetry
                 end
               end
             end
+
             app.use(*OpenTelemetry::Instrumentation::Rack::Instrumentation.instance.middleware_args)
             app.use(Middlewares::TracerMiddleware)
           end
