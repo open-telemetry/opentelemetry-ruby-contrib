fd7bcfd6e56571e128b5332702bd1db2ce4ca31a
Ariel Valentin <arielvalentin@users.noreply.github.com>
2023-09-23 14:34:24 +0000

    spike: Rewrite using AS notifications
    This is a spike to see if we can rewrite the ActiveJob instrumentation using AS notifications.
    
    Some things that stand out to me:
    
    Each subscriber requires additional logic to handle context propagation, which makes code reuse a little when clunky creating spans:
    - Egress (kind: consumer) spans must extract incoming context to continue the trace context or use links
      - This means keeping track of multiple context tokens and detaching them in reverse order
    - Ingress (kind: producer) spans must use the implicit parent span from the current context and must inject the outbound context
    
    - Not all events measure a block of code e.g. https://github.com/rails/rails/blob/a5f4aadcee42f9b4fa99468776c56b5b988c87cf/activejob/lib/active_job/instrumentation.rb#L31
    - Exceptions are not documented as being included in `perform` events, which means we may not have access to them to set an error on the Ingress spans
      - Exceptions seem to be caught and recorded as part of other events: https://github.com/rails/rails/blob/a5f4aadcee42f9b4fa99468776c56b5b988c87cf/activejob/lib/active_job/exceptions.rb#L152
      - `retry_stopped`, `discard`, and `enqueue_retry` will provide errors but should these be mapped to internal spans?
      - Would others agree that if the job is stopped or discarded due to an error, we should record the error?
    - There are no semantics around retries. I assume that means we do not need to track those?

--- instrumentation/active_job/example/active_job.rb
+++ instrumentation/active_job/example/active_job.rb
@@ -5,14 +5,21 @@
 # SPDX-License-Identifier: Apache-2.0
 
 require 'rubygems'
-require 'bundler/setup'
-require 'active_job'
+require 'bundler/inline'
 
-Bundler.require
+gemfile do
+  source 'https://rubygems.org'
+  gem 'activejob', require: 'active_job'
+  # gem 'opentelemetry-instrumentation-active_job', path: '../'
+  gem 'opentelemetry-sdk'
+end
+
+require_relative '../lib/opentelemetry/instrumentation/active_job/subscriber'
 
 ENV['OTEL_TRACES_EXPORTER'] ||= 'console'
 OpenTelemetry::SDK.configure do |c|
-  c.use 'OpenTelemetry::Instrumentation::ActiveJob'
+  # c.use 'OpenTelemetry::Instrumentation::ActiveJob'
+  at_exit { OpenTelemetry.tracer_provider.shutdown }
 end
 
 class TestJob < ::ActiveJob::Base
@@ -30,4 +37,4 @@ end
 ::ActiveJob::Base.queue_adapter = :async
 
 TestJob.perform_later
-sleep 0.1 # To ensure we see both spans!
+sleep 0.1 # allow the job to complete
---
+++ instrumentation/active_job/lib/opentelemetry/instrumentation/active_job/subscriber.rb
@@ -0,0 +1,208 @@
+# frozen_string_literal: true
+
+require 'active_support/subscriber'
+
+module OpenTelemetry
+  module Instrumentation
+    module ActiveJob
+      module Patches
+        # Module to prepend to ActiveJob::Core for context propagation.
+        module Base
+          def self.prepended(base)
+            base.class_eval do
+              attr_accessor :__otel_headers
+            end
+          end
+
+          def initialize(...)
+            @__otel_headers = {}
+            super
+          end
+
+          def serialize
+            message = super
+
+            begin
+              message.merge!('__otel_headers' => serialize_arguments(@__otel_headers))
+            rescue StandardError => error
+              OpenTelemetry.handle_error(exception: error)
+            end
+
+            message
+          end
+
+          def deserialize(job_data)
+            begin
+              @__otel_headers = deserialize_arguments(job_data.delete('__otel_headers') || []).to_h
+            rescue StandardError => error
+              OpenTelemetry.handle_error(exception: error)
+            end
+            super
+          end
+          ::ActiveJob::Base.prepend(self)
+        end
+      end
+    end
+  end
+end
+
+module OpenTelemetry
+  module Instrumentation
+    module ActiveJob
+      class EnqueueSubscriber < ::ActiveSupport::Subscriber
+        TEST_ADAPTERS = %w[async inline]
+
+        attach_to :active_job
+
+        # The methods below are the events the Subscriber is interested in.
+        def enqueue(...); end
+
+        def start(name, _id, payload)
+          begin
+            span = tracer.start_span("#{payload.fetch(:job).queue_name} publish",
+                                      kind: :producer,
+                                      attributes: job_attributes(payload.fetch(:job)))
+            tokens = [OpenTelemetry::Context.attach(OpenTelemetry::Trace.context_with_span(span))]
+            OpenTelemetry.propagation.inject(payload.fetch(:job).__otel_headers) # This must be transmitted over the wire
+            payload.merge!(__otel: { span: span, ctx_tokens: tokens }) # The payload is _not_ transmitted over the wire
+
+          rescue StandardError => error
+            OpenTelemetry.handle_error(exception: error)
+          end
+
+          super
+        end
+
+        def finish(_name, _id, payload)
+          begin
+            otel = payload.delete(:__otel)
+            span = otel.fetch(:span)
+            tokens = otel.fetch(:ctx_tokens)
+          rescue StandardError => error
+            OpenTelemetry.handle_error(exception: error)
+          end
+
+          super
+
+        ensure
+          begin
+            span&.finish
+          rescue StandardError => error
+            OpenTelemetry.handle_error(exception: error)
+          end
+          tokens&.reverse&.each do |token|
+            begin
+              OpenTelemetry::Context.detach(token)
+            rescue StandardError => error
+              OpenTelemetry.handle_error(exception: error)
+            end
+          end
+        end
+
+        private
+
+        def job_attributes(job)
+          otel_attributes = {
+            'code.namespace' => job.class.name,
+            'messaging.destination_kind' => 'queue',
+            'messaging.system' => job.class.queue_adapter_name,
+            'messaging.destination' => job.queue_name,
+            'messaging.message_id' => job.job_id,
+            'messaging.active_job.provider_job_id' => job.provider_job_id,
+            'messaging.active_job.priority' => job.priority
+          }
+
+          otel_attributes['net.transport'] = 'inproc' if TEST_ADAPTERS.include?(job.class.queue_adapter_name)
+          otel_attributes.compact!
+
+          otel_attributes
+        end
+
+        def tracer
+          OpenTelemetry.tracer_provider.tracer('otel-active_job', '0.0.1')
+        end
+      end
+
+      class PerformSubscriber < ::ActiveSupport::Subscriber
+        TEST_ADAPTERS = %w[async inline]
+
+        attach_to :active_job
+
+        # The methods below are the events the Subscriber is interested in.
+        def perform(...);end
+
+        def start(name, _id, payload)
+          tokens = []
+          begin
+            parent_context = OpenTelemetry.propagation.extract(payload.fetch(:job).__otel_headers)
+            span_context = OpenTelemetry::Trace.current_span(parent_context).context
+
+            if span_context.valid?
+              tokens << OpenTelemetry::Context.attach(parent_context)
+              links = [OpenTelemetry::Trace::Link.new(span_context)]
+            end
+
+            span = tracer.start_span("#{payload.fetch(:job).queue_name} process", kind: :consumer, attributes: job_attributes(payload.fetch(:job)), links: links)
+            tokens << OpenTelemetry::Context.attach(
+              OpenTelemetry::Trace.context_with_span(span)
+            )
+            payload.merge!(__otel: { span: span, ctx_tokens: tokens })
+          rescue StandardError => error
+            OpenTelemetry.handle_error(exception: error)
+          end
+
+          super
+        end
+
+        def finish(_name, _id, payload)
+          begin
+            otel = payload.delete(:__otel)
+            span = otel.fetch(:span)
+            tokens = otel.fetch(:ctx_tokens)
+          rescue StandardError => error
+            OpenTelemetry.handle_error(exception: error)
+          end
+
+          super
+
+        ensure
+          begin
+            span&.finish
+          rescue StandardError => error
+            OpenTelemetry.handle_error(exception: error)
+          end
+          tokens&.reverse&.each do |token|
+            begin
+              OpenTelemetry::Context.detach(token)
+            rescue StandardError => error
+              OpenTelemetry.handle_error(exception: error)
+            end
+          end
+        end
+
+        private
+
+        def job_attributes(job)
+          otel_attributes = {
+            'code.namespace' => job.class.name,
+            'messaging.destination_kind' => 'queue',
+            'messaging.system' => job.class.queue_adapter_name,
+            'messaging.destination' => job.queue_name,
+            'messaging.message_id' => job.job_id,
+            'messaging.active_job.provider_job_id' => job.provider_job_id,
+            'messaging.active_job.priority' => job.priority
+          }
+
+          otel_attributes['net.transport'] = 'inproc' if TEST_ADAPTERS.include?(job.class.queue_adapter_name)
+          otel_attributes.compact!
+
+          otel_attributes
+        end
+
+        def tracer
+          OpenTelemetry.tracer_provider.tracer('otel-active_job', '0.0.1')
+        end
+      end
+    end
+  end
+end
