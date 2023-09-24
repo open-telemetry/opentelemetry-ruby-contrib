c2e8bfc3980f8eaa23a8dfa93f4bff586ab215a8
Ariel Valentin <arielvalentin@users.noreply.github.com>
2023-09-24 15:09:57 +0000

    feat: add more details to demo

--- instrumentation/active_job/example/active_job.rb
+++ instrumentation/active_job/example/active_job.rb
@@ -4,6 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+ENV['OTEL_SERVICE_NAME'] ||= 'otel-active-job-demo'
 require 'rubygems'
 require 'bundler/inline'
 
@@ -12,6 +13,7 @@ gemfile do
   gem 'activejob', require: 'active_job'
   # gem 'opentelemetry-instrumentation-active_job', path: '../'
   gem 'opentelemetry-sdk'
+  gem 'opentelemetry-exporter-otlp'
 end
 
 require_relative '../lib/opentelemetry/instrumentation/active_job/subscriber'
@@ -20,6 +22,53 @@ ENV['OTEL_TRACES_EXPORTER'] ||= 'console'
 OpenTelemetry::SDK.configure do |c|
   # c.use 'OpenTelemetry::Instrumentation::ActiveJob'
   at_exit { OpenTelemetry.tracer_provider.shutdown }
+end
+
+class FailingJob < ::ActiveJob::Base
+  queue_as :demo
+  def perform
+    raise 'this job failed'
+  end
+end
+
+class FailingRetryJob < ::ActiveJob::Base
+  queue_as :demo
+
+  retry_on StandardError, attempts: 2, wait: 0
+  def perform
+    raise 'this job failed'
+  end
+end
+
+class RetryJob < ::ActiveJob::Base
+  queue_as :demo
+
+  retry_on StandardError, attempts: 3, wait: 0
+  def perform
+    if executions < 3
+      raise 'this job failed'
+    else
+      puts <<~EOS
+
+      --------------------------------------------------
+       Done Retrying!
+      --------------------------------------------------
+
+      EOS
+    end
+  end
+end
+
+class DiscardJob < ::ActiveJob::Base
+  queue_as :demo
+
+  class DiscardError < StandardError; end
+
+  discard_on DiscardError
+
+  def perform
+    raise DiscardError, 'this job failed'
+  end
 end
 
 class TestJob < ::ActiveJob::Base
@@ -34,7 +83,31 @@ class TestJob < ::ActiveJob::Base
   end
 end
 
+class DoItNowJob < ::ActiveJob::Base
+  def perform
+    puts <<~EOS
+
+    --------------------------------------------------
+     Called with perform_now!
+    --------------------------------------------------
+
+    EOS
+  end
+end
+
+class BatchJob < ::ActiveJob::Base
+  def perform
+    TestJob.perform_later
+    FailingJob.perform_later
+    FailingRetryJob.perform_later
+    RetryJob.perform_later
+    DiscardJob.perform_later
+  end
+end
+
 ::ActiveJob::Base.queue_adapter = :async
 
-TestJob.perform_later
-sleep 0.1 # allow the job to complete
+DoItNowJob.perform_now
+BatchJob.perform_later
+
+sleep 1 # allow the job to complete
