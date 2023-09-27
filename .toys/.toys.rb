48baa03246865db68a5804bcc4147ca059aacc79
Ariel Valentin <arielvalentin@github.com>
2023-09-26 20:01:22 -0500

    chore: use stable version of toys release code

--- .toys/.toys.rb
+++ .toys/.toys.rb
@@ -3,4 +3,5 @@ toys_version! "0.14.7"
 load_git remote: "https://github.com/dazuma/toys.git",
          path: ".toys/release",
          as: "release",
+         commit: "toys/v0.14.7",
          update: 3600
