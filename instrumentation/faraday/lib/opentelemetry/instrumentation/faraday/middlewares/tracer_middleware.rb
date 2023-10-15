4382e4106ea2b181426066dcd604a81532ce5c59
Yohei Kitamura <3087402+yoheyk@users.noreply.github.com>
2023-10-15 18:30:35 -0400

    fix: Omit `nil` `net.peer.name` attributes

--- instrumentation/faraday/lib/opentelemetry/instrumentation/faraday/middlewares/tracer_middleware.rb
+++ instrumentation/faraday/lib/opentelemetry/instrumentation/faraday/middlewares/tracer_middleware.rb
@@ -44,9 +44,9 @@ module OpenTelemetry
           def span_creation_attributes(http_method:, url:)
             instrumentation_attrs = {
               'http.method' => http_method,
-              'http.url' => OpenTelemetry::Common::Utilities.cleanse_url(url.to_s),
-              'net.peer.name' => url.host
+              'http.url' => OpenTelemetry::Common::Utilities.cleanse_url(url.to_s)
             }
+            instrumentation_attrs['net.peer.name'] = url.host if url.host
             config = Faraday::Instrumentation.instance.config
             instrumentation_attrs['peer.service'] = config[:peer_service] if config[:peer_service]
             instrumentation_attrs.merge!(
