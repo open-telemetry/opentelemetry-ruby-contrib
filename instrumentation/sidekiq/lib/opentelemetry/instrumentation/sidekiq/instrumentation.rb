1c10d02787b09bf5e4acd44e4235260262bae559
kohbis <18735471+kohbis@users.noreply.github.com>
2024-01-18 21:37:31 +0900

    docs: ✏️ Sidekiq instrumentation options (#824)

--- instrumentation/sidekiq/lib/opentelemetry/instrumentation/sidekiq/instrumentation.rb
+++ instrumentation/sidekiq/lib/opentelemetry/instrumentation/sidekiq/instrumentation.rb
@@ -27,13 +27,59 @@ module OpenTelemetry
           gem_version >= MINIMUM_VERSION
         end
 
+        # @!group Instrumentation Options
+        # @!macro
+        #   @!method $1
+        #   @instrumentation_option_default `:$2`
+        #   @!scope class
+        #
+        #   Specify how the span names are set. Can be one of:
+        #
+        #   - `:queue` - the span names will be set to '<destination / queue name> <operation>'.
+        #   - `:job_class` - the span names will be set to '<job class name> <operation>'.
         option :span_naming,                 default: :queue, validate: %I[job_class queue]
+        #   Controls how the job's execution is traced and related
+        #   to the trace where the job was enqueued. Can be one of:
+        #
+        #   - `:link` - the job will be executed in a separate trace. The
+        #     initial span of the execution trace will be linked to the span that
+        #     enqueued the job, via a Span Link.
+        #   - `:child` - the job will be executed in the same logical trace, as a direct
+        #     child of the span that enqueued the job.
+        #   - `:none` - the job's execution will not be explicitly linked to the span that
+        #     enqueued the job.
         option :propagation_style,           default: :link,  validate: %i[link child none]
+        # @!macro
+        #   @!method $1
+        #   @instrumentation_option_default `:$2`
+        #   @!scope class
+        #   Allows tracing Sidekiq::Launcher#heartbeat.
         option :trace_launcher_heartbeat,    default: false, validate: :boolean
+        # @!macro
+        #   @!method $1
+        #   @instrumentation_option_default $2
+        #   @!scope class
+        #   Allows tracing Sidekiq::Scheduled#enqueue.
         option :trace_poller_enqueue,        default: false, validate: :boolean
+        # @!macro
+        #   @!method $1
+        #   @instrumentation_option_default $2
+        #   @!scope class
+        #   Allows trasing Sidekiq::Scheduled#wait
         option :trace_poller_wait,           default: false, validate: :boolean
+        # @!macro
+        #   @!method $1
+        #   @instrumentation_option_default $2
+        #   @!scope class
+        #   Allows tracing Sidekiq::Processor#process_one.
         option :trace_processor_process_one, default: false, validate: :boolean
+        # @!macro
+        #   @!method $1
+        #   @instrumentation_option_default $2
+        #   @!scope class
+        #   Sets service name of the remote service.
         option :peer_service,                default: nil,   validate: :string
+        # @!endgroup
 
         private
 
