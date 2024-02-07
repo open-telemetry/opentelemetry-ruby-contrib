24fe20de136576106302cd0c77c151ac5fe5eb61
Ariel Valentin <arielvalentin@github.com>
2024-02-07 11:31:25 -0600

    chore: bump toys to v0.15.5
    dependabot cannot see these updates

--- .toys/.toys.rb
+++ .toys/.toys.rb
@@ -3,5 +3,5 @@ toys_version! "0.15.3"
 load_git remote: "https://github.com/dazuma/toys.git",
          path: ".toys/release",
          as: "release",
-         commit: "toys/v0.15.3",
+         commit: "toys/v0.15.5",
          update: 3600
