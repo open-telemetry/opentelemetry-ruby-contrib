6762619a04a1b84bbc6daef7bf925fc415b0b95c
Ariel Valentin <arielvalentin@users.noreply.github.com>
2024-05-13 10:31:03 -0500

    ci: Add ActionMailer CI (#976)
    * ci: Add ActionMailer CI
    
    * ci: Linter fixes
    
    * squash: drop pry-byebug

--- instrumentation/action_mailer/lib/opentelemetry/instrumentation/action_mailer/instrumentation.rb
+++ instrumentation/action_mailer/lib/opentelemetry/instrumentation/action_mailer/instrumentation.rb
@@ -10,7 +10,7 @@ module OpenTelemetry
       # The Instrumentation class contains logic to detect and install the ActionMailer instrumentation
       class Instrumentation < OpenTelemetry::Instrumentation::Base
         MINIMUM_VERSION = Gem::Version.new('6.1.0')
-        EMAIL_ATTRIBUTE = ['email.to.address', 'email.from.address', 'email.cc.address', 'email.bcc.address']
+        EMAIL_ATTRIBUTE = %w[email.to.address email.from.address email.cc.address email.bcc.address].freeze
 
         install do |_config|
           resolve_email_address
