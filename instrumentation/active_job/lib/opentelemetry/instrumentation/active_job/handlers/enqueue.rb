7305fa330c4efe00b2838b2ef5c8a6d7c1cddfb9
Sébastien Lavoie <sebastien.lavoie@shopify.com>
2024-07-22 10:32:59 -0400

    fix(active-job): honour dynamic changes in configuration

--- instrumentation/active_job/lib/opentelemetry/instrumentation/active_job/handlers/enqueue.rb
+++ instrumentation/active_job/lib/opentelemetry/instrumentation/active_job/handlers/enqueue.rb
@@ -10,15 +10,6 @@ module OpenTelemetry
       module Handlers
         # Handles `enqueue.active_job` and `enqueue_at.active_job` to generate egress spans
         class Enqueue < Default
-          def initialize(...)
-            super
-            @span_name_formatter = if @config[:span_naming] == :job_class
-                                     ->(job) { "#{job.class.name} publish" }
-                                   else
-                                     ->(job) { "#{job.queue_name} publish" }
-                                   end
-          end
-
           # Overrides the `Default#start_span` method to create an egress span
           # and registers it with the current context
           #
@@ -28,9 +19,21 @@ module OpenTelemetry
           # @return [Hash] with the span and generated context tokens
           def start_span(name, _id, payload)
             job = payload.fetch(:job)
-            span = tracer.start_span(@span_name_formatter.call(job), kind: :producer, attributes: @mapper.call(payload))
+            span = tracer.start_span(span_name(job), kind: :producer, attributes: @mapper.call(payload))
             OpenTelemetry.propagation.inject(job.__otel_headers) # This must be transmitted over the wire
             { span: span, ctx_token: OpenTelemetry::Context.attach(OpenTelemetry::Trace.context_with_span(span)) }
+          end
+
+          private
+
+          def span_name(job)
+            prefix = if @config[:span_naming] == :job_class
+                       job.class.name
+                     else
+                       job.queue_name
+                     end
+
+            "#{prefix} publish"
           end
         end
       end
