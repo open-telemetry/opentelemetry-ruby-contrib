ef099c3863356c68be0f2d7c46ab2a49637a965d
Ariel Valentin <arielvalentin@github.com>
2024-07-31 22:05:27 -0500

    squash: pr feedback

--- instrumentation/action_view/lib/opentelemetry/instrumentation/action_view/railtie.rb
+++ instrumentation/action_view/lib/opentelemetry/instrumentation/action_view/railtie.rb
@@ -19,15 +19,15 @@ module OpenTelemetry
         config.after_initialize do
           ::OpenTelemetry::Instrumentation::ActiveSupport::Instrumentation.instance.install({})
 
-          otel_config = ::OpenTelemetry::Instrumentation::ActionView::Instrumentation.instance.config
-          span_name_formatter = otel_config[:legacy_span_names] ? ::OpenTelemetry::Instrumentation::ActiveSupport::LEGACY_NAME_FORMATTER : nil
+          instance = ::OpenTelemetry::Instrumentation::ActionView::Instrumentation.instance
+          span_name_formatter = instance.config[:legacy_span_names] ? ::OpenTelemetry::Instrumentation::ActiveSupport::LEGACY_NAME_FORMATTER : nil
 
           SUBSCRIPTIONS.each do |subscription_name|
             ::OpenTelemetry::Instrumentation::ActiveSupport.subscribe(
-              ::OpenTelemetry::Instrumentation::ActionView::Instrumentation.instance.tracer,
+              instance.tracer,
               subscription_name,
-              otel_config[:notification_payload_transform],
-              otel_config[:disallowed_notification_payload_keys],
+              instance.config[:notification_payload_transform],
+              instance.config[:disallowed_notification_payload_keys],
               span_name_formatter: span_name_formatter
             )
           end
