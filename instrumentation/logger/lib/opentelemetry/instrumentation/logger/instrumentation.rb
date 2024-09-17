97b42deff5eafba3d97093c0f8ab51e5ede20c15
Kayla Reopelle <kreopelle@newrelic.com>
2024-09-17 15:15:40 -0700

    docs: add some config info

--- instrumentation/logger/lib/opentelemetry/instrumentation/logger/instrumentation.rb
+++ instrumentation/logger/lib/opentelemetry/instrumentation/logger/instrumentation.rb
@@ -7,7 +7,38 @@
 module OpenTelemetry
   module Instrumentation
     module Logger
-      # The Instrumentation class contains logic to detect and install the Logger instrumentation
+      # The `OpenTelemetry::Instrumentation::Logger::Instrumentation` class contains logic to detect and install the
+      # Ruby Logger library instrumentation.
+      #
+      # Installation and configuration of this instrumentation is done within the
+      # {https://www.rubydoc.info/gems/opentelemetry-sdk/OpenTelemetry/SDK#configure-instance_method OpenTelemetry::SDK#configure}
+      # block, calling {https://www.rubydoc.info/gems/opentelemetry-sdk/OpenTelemetry%2FSDK%2FConfigurator:use use()}
+      # or {https://www.rubydoc.info/gems/opentelemetry-sdk/OpenTelemetry%2FSDK%2FConfigurator:use_all use_all()}.
+      #
+      # ## Configuration keys and options
+      #
+      # ### `:name`
+      #
+      # Sets the name of the OpenTelemetry Logger InstrumentationScope.
+      #
+      # - The name of this gem, `'opentelemetry-instrumentation-logger'` is the default.
+      #
+      # ### `:version`
+      #
+      # Sets the version of the OpenTelemetry Logger InstrumentationScope.
+      #
+      # - This gem's current version is the default.
+      #
+      #
+      # @example An explicit default configuration
+      #   OpenTelemetry::SDK.configure do |c|
+      #     c.use_all({
+      #       'OpenTelemetry::Instrumentation::Sidekiq' => {
+      #         name: 'opentelemetry-instrumentation-logger',
+      #         version: '0.1.0'
+      #       },
+      #     })
+      #   end
       class Instrumentation < OpenTelemetry::Instrumentation::Base
         install do |_config|
           require_dependencies
