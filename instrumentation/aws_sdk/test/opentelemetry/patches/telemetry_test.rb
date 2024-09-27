c90c807532294ef93a4b62881e72350e77bae74d
Juli Tera <terajul@amazon.com>
2024-09-27 12:18:20 -0700

    Fix CI failure

--- instrumentation/aws_sdk/test/opentelemetry/patches/telemetry_test.rb
+++ instrumentation/aws_sdk/test/opentelemetry/patches/telemetry_test.rb
@@ -38,9 +38,6 @@ describe OpenTelemetry::Instrumentation::AwsSdk do
 
     describe 'Lambda' do
       let(:service_name) { 'Lambda' }
-      let(:service_uri) do
-        'https://lambda.us-east-1.amazonaws.com/2015-03-31/functions/'
-      end
       let(:client) do
         Aws::Lambda::Client.new(
           telemetry_provider: otel_provider,
@@ -82,7 +79,17 @@ describe OpenTelemetry::Instrumentation::AwsSdk do
 
       it 'creates spans with all the non-stubbed parameters' do
         skip unless TestHelper.telemetry_plugin?(service_name)
-        stub_request(:get, service_uri)
+        WebMock.disable_net_connect!
+        stub_request(:get, 'http://169.254.169.254/latest/api/token')
+          .with(
+            headers: {
+              'Accept' => '*/*',
+              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'Host' => '169.254.169.254',
+              'Metadata' => 'true',
+              'User-Agent' => 'Ruby'
+            }
+          ).to_return(status: 200, body: '', headers: {})
 
         client = Aws::Lambda::Client.new(telemetry_provider: otel_provider)
         client.list_functions
@@ -97,7 +104,7 @@ describe OpenTelemetry::Instrumentation::AwsSdk do
 
       it 'should have correct span attributes when error' do
         skip unless TestHelper.telemetry_plugin?(service_name)
-        stub_request(:get, service_uri).to_return(status: 400)
+        stub_request(:get, 'foo').to_return(status: 400)
 
         begin
           client.list_functions
