ee4690a29ef3cefe344249d8853e6be72e89e275
Ariel Valentin <arielvalentin@github.com>
2024-11-18 22:18:57 -0600

    squash: remove global state causing routes to be stale

--- instrumentation/action_pack/test/test_helpers/app_config.rb
+++ instrumentation/action_pack/test/test_helpers/app_config.rb
@@ -4,8 +4,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-class Application < Rails::Application; end
 require 'action_controller/railtie'
+class Application < Rails::Application; end
 require_relative 'middlewares'
 require_relative 'controllers'
 require_relative 'routes'
