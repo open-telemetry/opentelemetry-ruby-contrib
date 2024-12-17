813716f809f35e8e145867ca757bd22d1f8a7d63
Michał Kaźmierczak <michalkazmierczakpoz@gmail.com>
2024-12-17 23:01:39 +0100

    Update instrumentation/grpc/lib/opentelemetry/instrumentation/grpc/patches/client_stub.rb
    Co-authored-by: Kayla Reopelle <87386821+kaylareopelle@users.noreply.github.com>

--- instrumentation/grpc/lib/opentelemetry/instrumentation/grpc/patches/client_stub.rb
+++ instrumentation/grpc/lib/opentelemetry/instrumentation/grpc/patches/client_stub.rb
@@ -8,7 +8,7 @@ module OpenTelemetry
   module Instrumentation
     module Grpc
       module Patches
-        # Module to be prepended to force GRPC to use the client interceptor by
+        # Module to be prepended to force gRPC to use the client interceptor by
         # default so the user doesn't have to manually add it when initializing a client.
         module ClientStub
           def initialize(host, creds, **args)
