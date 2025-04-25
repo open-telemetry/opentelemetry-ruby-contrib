337f5432546c6814386a414ed25b466cd7fab43a
Michael He <53622546+yiyuan-he@users.noreply.github.com>
2025-04-25 15:28:36 -0700

    feat: contribute aws ecs resource detector (#1477)

--- resources/aws/test/opentelemetry/resource/detector/aws_test.rb
+++ resources/aws/test/opentelemetry/resource/detector/aws_test.rb
@@ -21,19 +21,157 @@ describe OpenTelemetry::Resource::Detector::AWS do
       stub_request(:get, 'http://169.254.169.254/latest/dynamic/instance-identity/document')
         .with(headers: { 'Accept' => '*/*' })
         .to_return(status: 404, body: 'Not Found')
+
+      # Clear environment variables for ECS
+      @original_env = ENV.to_hash
+      ENV.delete('ECS_CONTAINER_METADATA_URI')
+      ENV.delete('ECS_CONTAINER_METADATA_URI_V4')
     end
 
     after do
       WebMock.allow_net_connect!
+      ENV.replace(@original_env)
     end
 
-    let(:detected_resource) { detector.detect }
-    let(:detected_resource_attributes) { detected_resource.attribute_enumerator.to_h }
-    let(:expected_resource_attributes) { {} }
+    def assert_detection_result(detectors)
+      resource = detector.detect(detectors)
+      attributes = resource.attribute_enumerator.to_h
 
-    it 'returns an empty resource' do
-      _(detected_resource).must_be_instance_of(OpenTelemetry::SDK::Resources::Resource)
-      _(detected_resource_attributes).must_equal(expected_resource_attributes)
+      _(resource).must_be_instance_of(OpenTelemetry::SDK::Resources::Resource)
+      _(attributes).must_equal({})
+    end
+
+    it 'returns an empty resource with no detectors' do
+      assert_detection_result([])
+    end
+
+    it 'returns an empty resource when EC2 detection fails' do
+      assert_detection_result([:ec2])
+    end
+
+    it 'returns an empty resource when ECS detection fails' do
+      assert_detection_result([:ecs])
+    end
+
+    it 'returns an empty resource with unknown detector' do
+      assert_detection_result([:unknown])
+    end
+
+    it 'returns an empty resource with multiple detectors when all fail' do
+      assert_detection_result(%i[ec2 ecs unknown])
+    end
+
+    describe 'with successful EC2 detection' do
+      let(:token) { 'ec2-token-value' }
+      let(:identity_document) do
+        {
+          'instanceId' => 'i-1234567890abcdef0',
+          'instanceType' => 'm5.xlarge',
+          'accountId' => '123456789012',
+          'region' => 'us-west-2',
+          'availabilityZone' => 'us-west-2b'
+        }
+      end
+      let(:hostname) { 'ip-10-0-0-1.ec2.internal' }
+
+      before do
+        # Stub successful token request (IMDSv2)
+        stub_request(:put, 'http://169.254.169.254/latest/api/token')
+          .to_return(status: 200, body: token)
+
+        # Stub successful identity document request
+        stub_request(:get, 'http://169.254.169.254/latest/dynamic/instance-identity/document')
+          .to_return(status: 200, body: identity_document.to_json)
+
+        # Stub successful hostname request
+        stub_request(:get, 'http://169.254.169.254/latest/meta-data/hostname')
+          .with(headers: { 'X-aws-ec2-metadata-token' => token })
+          .to_return(status: 200, body: hostname)
+      end
+
+      it 'detects EC2 resources when specified' do
+        resource = detector.detect([:ec2])
+        attributes = resource.attribute_enumerator.to_h
+
+        _(attributes[OpenTelemetry::SemanticConventions::Resource::CLOUD_PROVIDER]).must_equal('aws')
+        _(attributes[OpenTelemetry::SemanticConventions::Resource::CLOUD_PLATFORM]).must_equal('aws_ec2')
+        _(attributes[OpenTelemetry::SemanticConventions::Resource::CLOUD_ACCOUNT_ID]).must_equal('123456789012')
+        _(attributes[OpenTelemetry::SemanticConventions::Resource::CLOUD_REGION]).must_equal('us-west-2')
+        _(attributes[OpenTelemetry::SemanticConventions::Resource::CLOUD_AVAILABILITY_ZONE]).must_equal('us-west-2b')
+        _(attributes[OpenTelemetry::SemanticConventions::Resource::HOST_ID]).must_equal('i-1234567890abcdef0')
+        _(attributes[OpenTelemetry::SemanticConventions::Resource::HOST_TYPE]).must_equal('m5.xlarge')
+        _(attributes[OpenTelemetry::SemanticConventions::Resource::HOST_NAME]).must_equal(hostname)
+      end
+
+      describe 'with succesefful ECS detection' do
+        let(:metadata_uri_v4) { 'http://169.254.170.2/v4' }
+        let(:container_metadata) do
+          {
+            'ContainerARN' => 'arn:aws:ecs:us-west-2:123456789012:container/container-id',
+            'LogDriver' => 'awslogs',
+            'LogOptions' => {
+              'awslogs-region' => 'us-west-2',
+              'awslogs-group' => 'my-log-group',
+              'awslogs-stream' => 'my-log-stream'
+            }
+          }
+        end
+
+        let(:task_metadata) do
+          {
+            'Cluster' => 'my-cluster',
+            'TaskARN' => 'arn:aws:ecs:us-west-2:123456789012:task/task-id',
+            'Family' => 'my-task-family',
+            'Revision' => '1',
+            'LaunchType' => 'FARGATE'
+          }
+        end
+
+        before do
+          ENV['ECS_CONTAINER_METADATA_URI_V4'] = metadata_uri_v4
+
+          # Stub container metadata endpoint
+          stub_request(:get, metadata_uri_v4)
+            .to_return(status: 200, body: container_metadata.to_json)
+
+          # Stub task metadata endpoint
+          stub_request(:get, "#{metadata_uri_v4}/task")
+            .to_return(status: 200, body: task_metadata.to_json)
+        end
+
+        it 'detects ECS resources when specified' do
+          # Properly stub the fetch_container_id method
+          OpenTelemetry::Resource::Detector::AWS::ECS.stub :fetch_container_id, '0123456789abcdef' * 4 do
+            # Also stub the hostname method
+            Socket.stub :gethostname, 'test-container' do
+              resource = detector.detect([:ecs])
+              attributes = resource.attribute_enumerator.to_h
+
+              _(attributes[OpenTelemetry::SemanticConventions::Resource::CLOUD_PROVIDER]).must_equal('aws')
+              _(attributes[OpenTelemetry::SemanticConventions::Resource::CLOUD_PLATFORM]).must_equal('aws_ecs')
+            end
+          end
+        end
+
+        it 'returns combined resources when multiple detectors are used' do
+          # Stub the container ID and hostname methods
+          OpenTelemetry::Resource::Detector::AWS::ECS.stub :fetch_container_id, '0123456789abcdef' * 4 do
+            Socket.stub :gethostname, 'test-container' do
+              # Mock EC2 detector to return a simple resource
+              ec2_resource = OpenTelemetry::SDK::Resources::Resource.create({ 'ec2.instance.id' => 'i-1234567890abcdef0' })
+              OpenTelemetry::Resource::Detector::AWS::EC2.stub :detect, ec2_resource do
+                resource = detector.detect(%i[ec2 ecs])
+                attributes = resource.attribute_enumerator.to_h
+
+                # Should include attributes from both detectors
+                _(attributes[OpenTelemetry::SemanticConventions::Resource::CLOUD_PROVIDER]).must_equal('aws')
+                _(attributes[OpenTelemetry::SemanticConventions::Resource::CLOUD_PLATFORM]).must_equal('aws_ecs')
+                _(attributes['ec2.instance.id']).must_equal('i-1234567890abcdef0')
+              end
+            end
+          end
+        end
+      end
     end
   end
 end
