d4002dfcfde5d5b82374c44f8be64d9cfc3ec995
joker1007 <kakyoin.hierophant@gmail.com>
2025-05-01 15:03:35 +0900

    Revert "fix: delete `each_batch` method from targets of instrumentation"
    This reverts commit de951233ef9a398323e5b5dcb29fbd3ca0f6a977.

--- instrumentation/rdkafka/lib/opentelemetry/instrumentation/rdkafka/patches/consumer.rb
+++ instrumentation/rdkafka/lib/opentelemetry/instrumentation/rdkafka/patches/consumer.rb
@@ -42,6 +42,34 @@ module OpenTelemetry
             end
           end
 
+          # each_batch method is deleted in rdkafka 0.20.0
+          if Gem::Version.new(::Rdkafka::VERSION) < Gem::Version.new('0.20.0')
+            def each_batch(max_items: 100, bytes_threshold: Float::INFINITY, timeout_ms: 250, yield_on_error: false, &block)
+              super do |messages, error|
+                if messages.empty?
+                  yield messages, error
+                else
+                  attributes = {
+                    'messaging.system' => 'kafka',
+                    'messaging.destination_kind' => 'topic',
+                    'messaging.kafka.message_count' => messages.size
+                  }
+
+                  links = messages.map do |message|
+                    trace_context = OpenTelemetry.propagation.extract(message.headers, getter: GETTER)
+                    span_context = OpenTelemetry::Trace.current_span(trace_context).context
+                    OpenTelemetry::Trace::Link.new(span_context) if span_context.valid?
+                  end
+                  links.compact!
+
+                  tracer.in_span('batch process', attributes: attributes, links: links, kind: :consumer) do
+                    yield messages, error
+                  end
+                end
+              end
+            end
+          end
+
           private
 
           def tracer
