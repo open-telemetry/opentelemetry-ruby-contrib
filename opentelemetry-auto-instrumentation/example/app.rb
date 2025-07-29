c478025918ec2f0d1c4e4121c4b01a04690b100f
xuan-cao-swi <xuan.cao@solarwinds.com>
2025-07-29 14:57:44 -0400

    reverse ci to original

--- opentelemetry-auto-instrumentation/example/app.rb
+++ opentelemetry-auto-instrumentation/example/app.rb
@@ -4,9 +4,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-require 'uri'
-require 'net/http'
-
 url = URI.parse("http://catfact.ninja/fact")
 req = Net::HTTP::Get.new(url.to_s)
 res = Net::HTTP.start(url.host, url.port) {|http|
