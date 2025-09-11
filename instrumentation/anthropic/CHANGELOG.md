3bbb2ae0a1fea4f54c1636209807f217495468c8
otelbot <197425009+otelbot@users.noreply.github.com>
2025-09-11 19:47:05 +0000

    release: Release opentelemetry-instrumentation-anthropic 0.1.0 (initial release)

--- instrumentation/anthropic/CHANGELOG.md
+++ instrumentation/anthropic/CHANGELOG.md
@@ -1,1 +1,5 @@
 # Release History: opentelemetry-instrumentation-anthropic
+
+### v0.1.0 / 2025-09-11
+
+Initial release.
--- instrumentation/anthropic/lib/opentelemetry/instrumentation/anthropic/version.rb
+++ instrumentation/anthropic/lib/opentelemetry/instrumentation/anthropic/version.rb
@@ -7,7 +7,7 @@
 module OpenTelemetry
   module Instrumentation
     module Anthropic
-      VERSION = '0.0.0'
+      VERSION = '0.1.0'
     end
   end
 end
