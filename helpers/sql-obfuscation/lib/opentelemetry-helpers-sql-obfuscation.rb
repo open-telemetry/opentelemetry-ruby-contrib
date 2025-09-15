b3b78777e6369699b733682a562366da438f7eea
Hannah Ramadan <hannahr315@gmail.com>
2025-09-15 11:47:02 -0700

    feat!: deprecate gem

--- helpers/sql-obfuscation/lib/opentelemetry-helpers-sql-obfuscation.rb
+++ helpers/sql-obfuscation/lib/opentelemetry-helpers-sql-obfuscation.rb
@@ -5,3 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 require_relative 'opentelemetry/helpers'
+
+OpenTelemetry.logger.warn <<~WARNING
+  [DEPRECATION] The 'opentelemetry-helpers-sql-obfuscation' is deprecated and has been replaced by 'opentelemetry-helpers-sql-processor'.
+  Please update your Gemfile to use 'opentelemetry-helpers-sql-processor' instead.
+WARNING
