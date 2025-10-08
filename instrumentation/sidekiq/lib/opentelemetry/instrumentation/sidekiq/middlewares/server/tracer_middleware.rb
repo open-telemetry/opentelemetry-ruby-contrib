open-telemetry/opentelemetry-ruby-contrib

commit f1dd08fb15f22ee89eb8c99748b21bb5d6c2fdb4

fix: add support for Sidekiq 8 (#1444)

instrumentation/sidekiq/lib/opentelemetry/instrumentation/sidekiq/middlewares/server/tracer_middleware.rb
@@ -31,11 +31,13 @@ module OpenTelemetry
                           end
 
               extracted_context = OpenTelemetry.propagation.extract(msg)
+              created_at = time_from_timestamp(msg['created_at'])
+              enqueued_at = time_from_timestamp(msg['created_at'])
               OpenTelemetry::Context.with_current(extracted_context) do
                 if instrumentation_config[:propagation_style] == :child
                   tracer.in_span(span_name, attributes: attributes, kind: :consumer) do |span|
-                    span.add_event('created_at', timestamp: msg['created_at'])
-                    span.add_event('enqueued_at', timestamp: msg['enqueued_at'])
+                    span.add_event('created_at', timestamp: created_at)
+                    span.add_event('enqueued_at', timestamp: enqueued_at)
                     yield
                   end
                 else
@@ -44,8 +46,8 @@ module OpenTelemetry
                   links << OpenTelemetry::Trace::Link.new(span_context) if instrumentation_config[:propagation_style] == :link && span_context.valid?
                   span = tracer.start_root_span(span_name, attributes: attributes, links: links, kind: :consumer)
                   OpenTelemetry::Trace.with_span(span) do
-                    span.add_event('created_at', timestamp: msg['created_at'])
-                    span.add_event('enqueued_at', timestamp: msg['enqueued_at'])
+                    span.add_event('created_at', timestamp: created_at)
+                    span.add_event('enqueued_at', timestamp: enqueued_at)
                     yield
                   rescue Exception => e # rubocop:disable Lint/RescueException
                     span.record_exception(e)
@@ -66,6 +68,15 @@ module OpenTelemetry
 
             def tracer
               Sidekiq::Instrumentation.instance.tracer
+            end
+
+            def time_from_timestamp(timestamp)
+              if timestamp.is_a?(Float)
+                # old format, timestamps were stored as fractional seconds since the epoch
+                timestamp
+              else
+                timestamp/1000r
+              end
             end
           end
         end

instrumentation/sidekiq/lib/opentelemetry/instrumentation/sidekiq/middlewares/client/tracer_middleware.rb
@@ -31,7 +31,7 @@ module OpenTelemetry
 
               tracer.in_span(span_name, attributes: attributes, kind: :producer) do |span|
                 OpenTelemetry.propagation.inject(job)
-                span.add_event('created_at', timestamp: job['created_at'])
+                span.add_event('created_at', timestamp: time_from_timestamp(job['created_at']))
                 yield
               end
             end
@@ -44,6 +44,15 @@ module OpenTelemetry
 
             def tracer
               Sidekiq::Instrumentation.instance.tracer
+            end
+
+            def time_from_timestamp(timestamp)
+              if timestamp.is_a?(Float)
+                # old format, timestamps were stored as fractional seconds since the epoch
+                timestamp
+              else
+                timestamp/1000r
+              end
             end
           end
         end

instrumentation/sidekiq/Appraisals
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.2.0')
+  appraise 'sidekiq-8.0' do
+    gem 'sidekiq', '~> 8.0'
+  end
+end
+
 appraise 'sidekiq-7.0' do
   gem 'sidekiq', '~> 7.0'
 end

instrumentation/sidekiq/test/opentelemetry/instrumentation/sidekiq/middlewares/client/tracer_middleware_test.rb
@@ -44,7 +44,10 @@ describe OpenTelemetry::Instrumentation::Sidekiq::Middlewares::Client::TracerMid
       _(enqueue_span.attributes['messaging.destination']).must_equal 'default'
       _(enqueue_span.attributes['messaging.destination_kind']).must_equal 'queue'
       _(enqueue_span.events.size).must_equal(1)
-      _(enqueue_span.events[0].name).must_equal('created_at')
+
+      created_event = enqueue_span.events[0]
+      _(created_event.name).must_equal('created_at')
+      _(created_event.timestamp.digits.count).must_equal(19)
     end
 
     it 'traces when enqueued with Active Job' do

instrumentation/sidekiq/test/opentelemetry/instrumentation/sidekiq/middlewares/server/tracer_middleware_test.rb
@@ -45,8 +45,14 @@ describe OpenTelemetry::Instrumentation::Sidekiq::Middlewares::Server::TracerMid
       _(job_span.attributes['messaging.operation']).must_equal 'process'
       _(job_span.attributes['peer.service']).must_be_nil
       _(job_span.events.size).must_equal(2)
-      _(job_span.events[0].name).must_equal('created_at')
-      _(job_span.events[1].name).must_equal('enqueued_at')
+
+      created_event = job_span.events[0]
+      _(created_event.name).must_equal('created_at')
+      _(created_event.timestamp.digits.count).must_equal(19)
+
+      enqueued_event = job_span.events[1]
+      _(enqueued_event.name).must_equal('enqueued_at')
+      _(enqueued_event.timestamp.digits.count).must_equal(19)
     end
 
     it 'traces when enqueued with Active Job' do

commit dc004078e6240590bb5da1262c33130146c96ffa

fix: use `enqueued_at` for `enqueued_at`
broken with https://github.com/open-telemetry/opentelemetry-ruby-contrib/pull/1444

instrumentation/sidekiq/lib/opentelemetry/instrumentation/sidekiq/middlewares/server/tracer_middleware.rb
@@ -32,7 +32,7 @@ module OpenTelemetry
 
               extracted_context = OpenTelemetry.propagation.extract(msg)
               created_at = time_from_timestamp(msg['created_at'])
-              enqueued_at = time_from_timestamp(msg['created_at'])
+              enqueued_at = time_from_timestamp(msg['enqueued_at'])
               scheduled_at = time_from_timestamp(msg['at']) unless msg['at'].nil?
               OpenTelemetry::Context.with_current(extracted_context) do
                 if instrumentation_config[:propagation_style] == :child