b78645e32a20b4dbd8c0d9be9866dc6f8861284a
Ariel Valentin <arielvalentin@github.com>
2025-10-11 18:06:52 -0500

    wip: I will come back to you

--- instrumentation/active_record/test/instrumentation/active_record/handlers_test.rb
+++ instrumentation/active_record/test/instrumentation/active_record/handlers_test.rb
@@ -6,49 +6,46 @@
 
 require 'test_helper'
 
-# Skip this entire test file if handlers aren't loaded
-if defined?(OpenTelemetry::Instrumentation::ActiveRecord::Handlers)
-  describe OpenTelemetry::Instrumentation::ActiveRecord::Handlers do
-    let(:instrumentation) { OpenTelemetry::Instrumentation::ActiveRecord::Instrumentation.instance }
+describe OpenTelemetry::Instrumentation::ActiveRecord::Handlers do
+  let(:instrumentation) { OpenTelemetry::Instrumentation::ActiveRecord::Instrumentation.instance }
 
-    describe '.subscribe' do
-      it 'subscribes to sql.active_record when enabled' do
-        skip 'notifications_instrumentation not enabled' unless instrumentation.config[:enable_notifications_instrumentation]
+  # describe '.subscribe' do
+  #   it 'subscribes to sql.active_record when enabled' do
+  #     skip 'notifications_instrumentation not enabled' unless instrumentation.config[:enable_notifications_instrumentation]
 
-        # Verify that subscription exists
-        subscribers = ActiveSupport::Notifications.notifier.listeners_for('sql.active_record')
-        
-        assert subscribers.any?, 'Expected to find subscribers for sql.active_record'
-      end
+  #     # Verify that subscription exists
+  #     subscribers = ActiveSupport::Notifications.notifier.listeners_for('sql.active_record')
 
-      it 'does not subscribe twice' do
-        skip 'notifications_instrumentation not enabled' unless instrumentation.config[:enable_notifications_instrumentation]
+  #     _(subscribers).wont_be_empty
+  #   end
 
-        initial_count = ActiveSupport::Notifications.notifier.listeners_for('sql.active_record').size
-        
-        OpenTelemetry::Instrumentation::ActiveRecord::Handlers.subscribe
-        
-        final_count = ActiveSupport::Notifications.notifier.listeners_for('sql.active_record').size
-        
-        assert_equal initial_count, final_count, 'Should not subscribe twice'
-      end
-    end
+  #   it 'does not subscribe twice' do
+  #     skip 'notifications_instrumentation not enabled' unless instrumentation.config[:enable_notifications_instrumentation]
 
-    describe '.unsubscribe' do
-      it 'removes subscriptions' do
-        skip 'notifications_instrumentation not enabled' unless instrumentation.config[:enable_notifications_instrumentation]
+  #     initial_count = ActiveSupport::Notifications.notifier.listeners_for('sql.active_record').size
 
-        initial_count = ActiveSupport::Notifications.notifier.listeners_for('sql.active_record').size
-        
-        OpenTelemetry::Instrumentation::ActiveRecord::Handlers.unsubscribe
-        
-        final_count = ActiveSupport::Notifications.notifier.listeners_for('sql.active_record').size
-        
-        assert final_count < initial_count, 'Should have fewer subscribers after unsubscribe'
-        
-        # Re-subscribe for other tests
-        OpenTelemetry::Instrumentation::ActiveRecord::Handlers.subscribe
-      end
-    end
-  end
+  #     OpenTelemetry::Instrumentation::ActiveRecord::Handlers.subscribe
+
+  #     final_count = ActiveSupport::Notifications.notifier.listeners_for('sql.active_record').size
+
+  #     _(final_count).must_equal initial_count
+  #   end
+  # end
+
+  # describe '.unsubscribe' do
+  #   it 'removes subscriptions' do
+  #     skip 'notifications_instrumentation not enabled' unless instrumentation.config[:enable_notifications_instrumentation]
+
+  #     initial_count = ActiveSupport::Notifications.notifier.listeners_for('sql.active_record').size
+
+  #     OpenTelemetry::Instrumentation::ActiveRecord::Handlers.unsubscribe
+
+  #     final_count = ActiveSupport::Notifications.notifier.listeners_for('sql.active_record').size
+
+  #     _(final_count).must_be :<, initial_count
+
+  #     # Re-subscribe for other tests
+  #     OpenTelemetry::Instrumentation::ActiveRecord::Handlers.subscribe
+  #   end
+  # end
 end
