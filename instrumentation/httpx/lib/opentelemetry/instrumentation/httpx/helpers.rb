cae8ddd5c4859bce4d75b536fd426c7566e27d6f
Ariel Valentin <arielvalentin@github.com>
2025-11-02 10:57:27 -0600

    squash: add documentation

--- instrumentation/httpx/lib/opentelemetry/instrumentation/httpx/helpers.rb
+++ instrumentation/httpx/lib/opentelemetry/instrumentation/httpx/helpers.rb
@@ -11,6 +11,12 @@ module OpenTelemetry
       module Helpers
         extend self
 
+        # Formats the span name based on the HTTP method and URL template if available
+        #
+        # @param attributes [Hash] The span attributes hash
+        # @param http_method [String] The HTTP request method (e.g., 'GET', 'POST')
+        # @return [String] The formatted span name
+        # @api private
         def format_span_name(attributes, http_method)
           template = attributes['url.template']
           template ? "#{http_method} #{template}" : http_method
