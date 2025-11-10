589a51a7173499dd28d426ccceda97f2e0798152
xuan-cao-swi <xuan.cao@solarwinds.com>
2025-11-10 12:44:18 -0500

    update net_http test case

--- instrumentation/net_http/test/opentelemetry/instrumentation/net/http/stable/metrics_test.rb
+++ instrumentation/net_http/test/opentelemetry/instrumentation/net/http/stable/metrics_test.rb
@@ -13,7 +13,18 @@ describe OpenTelemetry::Instrumentation::Net::HTTP::Instrumentation do
   let(:instrumentation) { OpenTelemetry::Instrumentation::Net::HTTP::Instrumentation.instance }
   let(:exporter) { EXPORTER }
   let(:span) { exporter.finished_spans.first }
-  # let(:metric_exporter) { METRICS_EXPORTER }
+
+  # Helper method to verify metric structure
+  def assert_duration_metric(metric, expected_count: nil)
+    _(metric).wont_be_nil
+    _(metric.name).must_equal 'http.client.request.duration'
+    _(metric.description).must_equal 'Duration of HTTP client requests.'
+    _(metric.unit).must_equal 'ms'
+    _(metric.instrument_kind).must_equal :histogram
+    _(metric.instrumentation_scope.name).must_equal 'OpenTelemetry::Instrumentation::Net::HTTP'
+    _(metric.data_points).wont_be_empty
+    _(metric.data_points.first.count).must_equal expected_count if expected_count
+  end
 
   before do
     skip unless ENV['BUNDLE_GEMFILE'].include?('stable')
@@ -47,52 +58,32 @@ describe OpenTelemetry::Instrumentation::Net::HTTP::Instrumentation do
   end
 
   describe 'metrics integration' do
-    it 'records metrics alongside spans for successful requests' do
+    it 'records metrics alongside spans for various scenarios' do
+      # Successful request
       Net::HTTP.get('example.com', '/success')
-
-      # Verify span was created
       _(exporter.finished_spans.size).must_equal 1
       _(span.name).must_equal 'GET'
 
-      # Pull and verify metrics
-      @metric_exporter.pull
-      metrics = @metric_exporter.metric_snapshots
-
-      _(metrics).wont_be_empty
-      duration_metric = metrics[0]
-
-      _(duration_metric).wont_be_nil
-      _(duration_metric.name).must_equal 'http.client.request.duration'
-      _(duration_metric.description).must_equal 'Duration of HTTP client requests.'
-      _(duration_metric.unit).must_equal 'ms'
-      _(duration_metric.instrument_kind).must_equal :histogram
-      _(duration_metric.instrumentation_scope.name).must_equal 'OpenTelemetry::Instrumentation::Net::HTTP'
-      _(duration_metric.data_points).wont_be_empty
-      _(duration_metric.data_points.first.count).must_equal 1
-    end
-
-    it 'records metrics for requests with different status codes' do
-      # Test successful request
-      Net::HTTP.get('example.com', '/success')
-
       @metric_exporter.pull
       metrics = @metric_exporter.metric_snapshots
       _(metrics).wont_be_empty
-      _(metrics[0].data_points.first.count).must_equal 1
+      assert_duration_metric(metrics[0], expected_count: 1)
 
-      # Test failed request
+      # Failed request with 500 status
       Net::HTTP.post(URI('http://example.com/failure'), 'q' => 'ruby')
-
       _(exporter.finished_spans.last.attributes['http.response.status_code']).must_equal 500
 
       @metric_exporter.pull
       metrics = @metric_exporter.metric_snapshots
-      duration_metric = metrics[0]
+      assert_duration_metric(metrics[0], expected_count: 1)
 
-      _(duration_metric).wont_be_nil
-      _(duration_metric.name).must_equal 'http.client.request.duration'
-      _(duration_metric.data_points).wont_be_empty
-      _(duration_metric.data_points.first.count).must_equal 1
+      # Multiple requests accumulate
+      2.times { Net::HTTP.get('example.com', '/success') }
+      _(exporter.finished_spans.size).must_equal 4
+
+      @metric_exporter.pull
+      metrics = @metric_exporter.metric_snapshots
+      assert_duration_metric(metrics[0], expected_count: 3)
     end
 
     it 'records metrics even when request times out' do
@@ -100,47 +91,12 @@ describe OpenTelemetry::Instrumentation::Net::HTTP::Instrumentation do
         Net::HTTP.get(URI('https://example.com/timeout'))
       end.must_raise Net::OpenTimeout
 
-      # Verify span was created
       _(exporter.finished_spans.size).must_equal 1
 
-      # Pull and verify metrics
       @metric_exporter.pull
       metrics = @metric_exporter.metric_snapshots
-
       _(metrics).wont_be_empty
-      duration_metric = metrics[0]
-
-      _(duration_metric).wont_be_nil
-      _(duration_metric.name).must_equal 'http.client.request.duration'
-      _(duration_metric.description).must_equal 'Duration of HTTP client requests.'
-      _(duration_metric.unit).must_equal 'ms'
-      _(duration_metric.instrument_kind).must_equal :histogram
-      _(duration_metric.data_points).wont_be_empty
-      _(duration_metric.data_points.first.count).must_equal 1
-    end
-
-    it 'records metrics for multiple requests' do
-      # Make multiple requests
-      3.times do
-        Net::HTTP.get('example.com', '/success')
-      end
-
-      # Pull and verify metrics
-      @metric_exporter.pull
-      metrics = @metric_exporter.metric_snapshots
-
-      _(metrics).wont_be_empty
-      _(exporter.finished_spans.size).must_equal 3
-
-      duration_metric = metrics[0]
-
-      _(duration_metric).wont_be_nil
-      _(duration_metric.name).must_equal 'http.client.request.duration'
-      _(duration_metric.description).must_equal 'Duration of HTTP client requests.'
-      _(duration_metric.unit).must_equal 'ms'
-      _(duration_metric.instrument_kind).must_equal :histogram
-      _(duration_metric.data_points).wont_be_empty
-      _(duration_metric.data_points.first.count).must_equal 3
+      assert_duration_metric(metrics[0], expected_count: 1)
     end
 
     it 'does not record metrics in untraced context' do
@@ -148,11 +104,9 @@ describe OpenTelemetry::Instrumentation::Net::HTTP::Instrumentation do
         Net::HTTP.get('example.com', '/success')
       end
 
-      # Pull metrics
       @metric_exporter.pull
       metrics = @metric_exporter.metric_snapshots
 
-      # Verify that no metrics were recorded in untraced context
       duration_metric = metrics.find { |m| m.name == 'http.client.request.duration' }
       _(duration_metric).must_be_nil
     end
@@ -183,66 +137,6 @@ describe OpenTelemetry::Instrumentation::Net::HTTP::Instrumentation do
     end
   end
 
-  describe '#connect metrics' do
-    it 'records metrics for connect operations' do
-      WebMock.allow_net_connect!
-
-      TCPServer.open('localhost', 0) do |server|
-        Thread.start { server.accept }
-        port = server.addr[1]
-
-        uri = URI.parse("http://localhost:#{port}/example")
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.read_timeout = 0
-
-        # This will connect and timeout on read
-        _(-> { http.request(Net::HTTP::Get.new(uri.request_uri)) }).must_raise(Net::ReadTimeout)
-      end
-
-      # Pull and verify metrics
-      @metric_exporter.pull
-      metrics = @metric_exporter.metric_snapshots
-
-      _(metrics).wont_be_empty
-      duration_metric = metrics[0]
-
-      _(duration_metric).wont_be_nil
-      _(duration_metric.name).must_equal 'http.client.request.duration'
-      _(duration_metric.description).must_equal 'Duration of HTTP client requests.'
-      _(duration_metric.unit).must_equal 'ms'
-      _(duration_metric.instrument_kind).must_equal :histogram
-      _(duration_metric.data_points).wont_be_empty
-    ensure
-      WebMock.disable_net_connect!
-    end
-  end
-
-  describe 'error handling' do
-    it 'continues to work after metric recording errors' do
-      # Mock the instrumentation instance to return nil config
-      Net::HTTP.class_eval do
-        alias_method :original_record_metric, :record_metric
-
-        define_method(:record_metric) do |_duration_ms|
-          # Simulate an error in metric recording
-          raise StandardError, 'Metric recording error'
-        end
-      end
-
-      # This should not raise an exception
-      Net::HTTP.get('example.com', '/success')
-
-      # Restore original method
-      Net::HTTP.class_eval do
-        alias_method :record_metric, :original_record_metric
-        remove_method :original_record_metric
-      end
-
-      # If we get here without an exception, the test passes
-      _(true).must_equal true
-    end
-  end
-
   describe 'untraced_hosts configuration' do
     before do
       stub_request(:get, 'http://ignored.com/body').to_return(status: 200)
@@ -260,11 +154,9 @@ describe OpenTelemetry::Instrumentation::Net::HTTP::Instrumentation do
     it 'does not record metrics for ignored hosts' do
       Net::HTTP.get('ignored.com', '/body')
 
-      # Pull metrics
       @metric_exporter.pull
       metrics = @metric_exporter.metric_snapshots
 
-      # Verify no metrics recorded for ignored host
       duration_metric = metrics.find { |m| m.name == 'http.client.request.duration' }
       _(duration_metric).must_be_nil
     end
@@ -272,20 +164,11 @@ describe OpenTelemetry::Instrumentation::Net::HTTP::Instrumentation do
     it 'records metrics for non-ignored hosts' do
       Net::HTTP.get('tracked.com', '/body')
 
-      # Pull and verify metrics
       @metric_exporter.pull
       metrics = @metric_exporter.metric_snapshots
 
       _(metrics).wont_be_empty
-      duration_metric = metrics[0]
-
-      _(duration_metric).wont_be_nil
-      _(duration_metric.name).must_equal 'http.client.request.duration'
-      _(duration_metric.description).must_equal 'Duration of HTTP client requests.'
-      _(duration_metric.unit).must_equal 'ms'
-      _(duration_metric.instrument_kind).must_equal :histogram
-      _(duration_metric.data_points).wont_be_empty
-      _(duration_metric.data_points.first.count).must_equal 1
+      assert_duration_metric(metrics[0], expected_count: 1)
     end
   end
 end
