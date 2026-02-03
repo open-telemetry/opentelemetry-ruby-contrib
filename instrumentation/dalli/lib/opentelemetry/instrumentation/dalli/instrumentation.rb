bec3b1f59ac9eee6333075fb7ddcfc56ae9d514e
Hannah Ramadan <hannahr315@gmail.com>
2026-02-03 14:41:02 -0800

    fix: skip instrumenting dalli 4.2.0

--- instrumentation/dalli/lib/opentelemetry/instrumentation/dalli/instrumentation.rb
+++ instrumentation/dalli/lib/opentelemetry/instrumentation/dalli/instrumentation.rb
@@ -30,12 +30,23 @@ module OpenTelemetry
         end
 
         def add_patches
+          if dalli_has_native_otel_support?
+            OpenTelemetry.logger.info("Dalli #{::Dalli::VERSION} has native OpenTelemetry support. Skipping community instrumentation") 
+
+            return
+          end
+
           if Gem::Version.new(::Dalli::VERSION) < Gem::Version.new('3.0.0')
             ::Dalli::Server.prepend(Patches::Server)
           else
             ::Dalli::Protocol::Binary.prepend(Patches::Server) if defined?(::Dalli::Protocol::Binary)
             ::Dalli::Protocol::Meta.prepend(Patches::Server) if defined?(::Dalli::Protocol::Meta)
           end
+        end
+
+        def dalli_has_native_otel_support?
+          # Dalli 4.2.0+ has native OpenTelemetry instrumentation
+          Gem::Version.new(::Dalli::VERSION) >= Gem::Version.new('4.2.0')
         end
       end
     end
