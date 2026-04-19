5e2806545657b286fc7f2429b1e2aca9481359e9
James Thompson <thompson.tomo@outlook.com>
2026-04-19 23:09:27 +1000

    change script due to libuv issues

--- bin/link-check.js
+++ bin/link-check.js
@@ -1,7 +1,12 @@
 // This script is necessary as we can't pass environment variables to
 // npm scripts in a cross platform manner.
-import { execSync } from "node:child_process";
+import { spawnSync } from "node:child_process";
 
 const cfg = process.env.lsconfig || "";
 
-execSync(`linkspector check ${cfg}`, { stdio: "inherit" });
+const result = spawnSync("linkspector", ["check", ...cfg.split(" ")], {
+  stdio: "inherit",
+  shell: true
+});
+
+process.exit(result.status ?? 1);
