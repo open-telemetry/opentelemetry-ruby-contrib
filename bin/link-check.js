4bf076e837039adb94e0eb688bbb0fd62a9aaf6b
James Thompson <thompson.tomo@outlook.com>
2026-04-19 22:52:04 +1000

    switch to script due to windows

---
+++ bin/link-check.js
@@ -0,0 +1,7 @@
+// This script is necessary as we can't pass environment variables to 
+// npm scripts in a cross platform manner.
+import { execSync } from "node:child_process";
+
+const cfg = process.env.lsconfig || "";
+
+execSync(`linkspector check ${cfg}`, { stdio: "inherit" });
