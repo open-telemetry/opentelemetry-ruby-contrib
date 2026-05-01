c9baf6ea2b35d2840bb857166811ed13f82592bd
James Thompson <thompson.tomo@outlook.com>
2026-05-01 12:32:37 +1000

    Fix test/ci

--- instrumentation/trilogy/test/opentelemetry/instrumentation/trilogy/instrumentation_test.rb
+++ instrumentation/trilogy/test/opentelemetry/instrumentation/trilogy/instrumentation_test.rb
@@ -73,10 +73,10 @@ describe OpenTelemetry::Instrumentation::Trilogy do
   describe '#compatible?' do
     describe 'when an unsupported version is installed' do
       it 'is incompatible' do
-        stub_const('Trilogy::VERSION', '2.2.0')
+        stub_const('Trilogy::VERSION', '2.10.0')
         _(instrumentation.compatible?).must_equal false
 
-        stub_const('Trilogy::VERSION', '2.3.0.beta')
+        stub_const('Trilogy::VERSION', '2.11.0.beta')
         _(instrumentation.compatible?).must_equal false
 
         stub_const('Trilogy::VERSION', '3.0.0')
@@ -86,7 +86,7 @@ describe OpenTelemetry::Instrumentation::Trilogy do
 
     describe 'when supported version is installed' do
       it 'is compatible' do
-        stub_const('Trilogy::VERSION', '2.3.0')
+        stub_const('Trilogy::VERSION', '2.11.0')
         _(instrumentation.compatible?).must_equal true
 
         stub_const('Trilogy::VERSION', '3.0.0.rc1')
