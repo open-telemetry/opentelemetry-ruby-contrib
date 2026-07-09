22d78eb97a76fd412cd766fca6ffc1955206bc22
Yutaka Hara <yutaka.hara+github@gmail.com>
2026-07-09 17:14:45 +0900

    feat: Add opentelemetry-resource-detector-os

---
+++ resources/os/test/opentelemetry/resource/detector/os_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Copyright The OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'test_helper'
+
+describe OpenTelemetry::Resource::Detector::OS do
+  let(:detector) { OpenTelemetry::Resource::Detector::OS }
+
+  RESOURCE = OpenTelemetry::SemanticConventions::Resource
+
+  describe '.detect' do
+    # TODO
+  end
+end
