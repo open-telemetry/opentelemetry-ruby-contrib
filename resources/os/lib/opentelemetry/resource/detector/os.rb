b5264ddf229a7e88e444efa69421ff0f42483181
Yutaka HARA <yutaka.hara+github@gmail.com>
2026-07-10 15:19:00 +0900

    Use OpenTelemetry::SemConv
    Co-authored-by: James Thompson <thompson.tomo@outlook.com>

--- resources/os/lib/opentelemetry/resource/detector/os.rb
+++ resources/os/lib/opentelemetry/resource/detector/os.rb
@@ -16,10 +16,10 @@ module OpenTelemetry
         def detect
           resource_attributes = {}
 
-          resource_attributes[OpenTelemetry::SemanticConventions::Resource::OS_TYPE] = get_os_type
-          resource_attributes[OpenTelemetry::SemanticConventions::Resource::OS_DESCRIPTION] = get_os_description
-          resource_attributes[OpenTelemetry::SemanticConventions::Resource::OS_NAME] = get_os_name
-          resource_attributes[OpenTelemetry::SemanticConventions::Resource::OS_VERSION] = get_os_version
+          resource_attributes[OpenTelemetry::SemConv::Incubating::OS::OS_TYPE] = get_os_type
+          resource_attributes[OpenTelemetry::SemConv::Incubating::OS::OS_DESCRIPTION] = get_os_description
+          resource_attributes[OpenTelemetry::SemConv::Incubating::OS::OS_NAME] = get_os_name
+          resource_attributes[OpenTelemetry::SemConv::Incubating::OS::OS_VERSION] = get_os_version
 
           OpenTelemetry::SDK::Resources::Resource.create(resource_attributes)
         end
