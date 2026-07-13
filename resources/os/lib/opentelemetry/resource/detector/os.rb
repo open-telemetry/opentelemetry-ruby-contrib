fd74899db99fb2edfc74c1b1262c83c565bcf8fc
Yutaka Hara <yutaka.hara+github@gmail.com>
2026-07-13 19:34:40 +0900

    feat: implement more os attributes

--- resources/os/lib/opentelemetry/resource/detector/os.rb
+++ resources/os/lib/opentelemetry/resource/detector/os.rb
@@ -4,6 +4,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+require 'open3'
+
 module OpenTelemetry
   module Resource
     module Detector
@@ -12,49 +14,92 @@ module OpenTelemetry
         extend self
 
         RESOURCE = OpenTelemetry::SDK::Resources::Resource
+        SEMCONV = OpenTelemetry::SemConv::Incubating::OS
 
         def detect
-          resource_attributes = {}
-
-          resource_attributes[OpenTelemetry::SemConv::Incubating::OS::OS_TYPE] = get_os_type
-          #resource_attributes[OpenTelemetry::SemConv::Incubating::OS::OS_DESCRIPTION] = get_os_description
-          #resource_attributes[OpenTelemetry::SemConv::Incubating::OS::OS_NAME] = get_os_name
-          #resource_attributes[OpenTelemetry::SemConv::Incubating::OS::OS_VERSION] = get_os_version
-          #resource_attributes[OpenTelemetry::SemConv::Incubating::OS::OS_BUILD_ID] = get_os_build_id
-
+          resource_attributes =
+            case target_os
+            when /linux/ then get_linux_attrs
+            when /darwin/ then get_macos_attrs
+            when /mswin|msys|mingw|cygwin/ then get_windows_attrs
+            end
           OpenTelemetry::SDK::Resources::Resource.create(resource_attributes)
         end
 
         private
 
-        def get_os_type
-          rbconfig = RbConfig::CONFIG['host_os']
-          case rbconfig
-          when /aix/                     then 'aix'
-          when /darwin/                  then 'darwin'
-          when /dragonfly/               then 'dragonflybsd'
-          when /freebsd/                 then 'freebsd'
-          when /hpux/                    then 'hpux'
-          when /linux/                   then 'linux'
-          when /netbsd/                  then 'netbsd'
-          when /openbsd/                 then 'openbsd'
-          when /solaris|sunos/           then 'solaris'
-          when /mswin|msys|mingw|cygwin/ then 'windows'
+        def target_os
+          RbConfig::CONFIG['target_os']
+        end
+
+        def get_linux_attrs
+          attrs = {
+            SEMCONV::OS_TYPE => 'linux'
+          }
+          os_release = get_linux_os_release
+          if os_release
+            # eg. "Ubuntu"
+            name = lookup_os_release(os_release, "NAME")
+            if name
+              attrs[SEMCONV::OS_NAME] = name
+            end
+            # eg. "26.04"
+            ver_id = lookup_os_release(os_release, "VERSION_ID")
+            if ver_id
+              attrs[SEMCONV::OS_VERSION] = ver_id
+            end
+            # eg. "Ubuntu 26.04 LTS"
+            pretty = lookup_os_release(os_release, "PRETTY_NAME")
+            if pretty
+              attrs[SEMCONV::OS_DESCRIPTION] = pretty
+            end
+            # eg. "7.0.0-1008-aws",
+            #     "5.15.146.1-microsoft-standard-WSL2+"
+            build_id = lookup_os_release(os_release, "BUILD_ID")
+            if build_id
+              attrs[SEMCONV::OS_BUILD_ID] = build_id
+            elsif File.readable?("/proc/sys/kernel/osrelease")
+              attrs[SEMCONV::OS_BUILD_ID] = File.read("/proc/sys/kernel/osrelease")
+            end
+          end
+          attrs
+        end
+
+        def get_linux_os_release
+          if File.readable?("/etc/os-release")
+            File.read("/etc/os-release")
+          elsif File.readable?("/usr/lib/os-release")
+            File.read("/usr/lib/os-release")
           else
-            rbconfig
+            nil
           end
         end
 
-        def get_os_description
+        def lookup_os_release(os_release, key)
+          os_release[/^#{key}=(.*)$/, 1]
         end
 
-        def get_os_name
+        def get_macos_attrs
+          {
+            SEMCONV::OS_TYPE => 'darwin',
+            SEMCONV::OS_NAME => 'macOS',
+          }
         end
 
-        def get_os_version
+        def get_windows_attrs
+          {
+            SEMCONV::OS_TYPE => 'windows',
+            SEMCONV::OS_NAME => 'Windows',
+            # eg. "10.0.26200"
+            SEMCONV::OS_VERSION => Etc.uname[:release],
+            # eg. "Microsoft Windows [Version 10.0.26200.8037]"
+            SEMCONV::OS_DESCRIPTION => get_windows_ver.strip,
+          }
         end
 
-        def get_os_build_id
+        def get_windows_ver
+          stdout, _stderr, _status = Open3.capture3("ver")
+          stdout
         end
       end
     end
--- resources/os/test/opentelemetry/resource/detector/os_test.rb
+++ resources/os/test/opentelemetry/resource/detector/os_test.rb
@@ -15,8 +15,37 @@ describe OpenTelemetry::Resource::Detector::OS do
     let(:detected_resource) { detector.detect }
     let(:detected_resource_attributes) { detected_resource.attribute_enumerator.to_h }
 
-    it 'returns a resource with os_type string' do
-      _(detected_resource_attributes['os.type']).must_be_instance_of(String)
+    describe 'on linux' do
+      # TODO
+    end
+    
+    describe 'on macOS' do
+      # TODO
+    end
+    
+    describe 'on windows' do
+      before do
+        allow(detector).to receive(:target_os).and_return("mingw32")
+        allow(Open3).to receive(:capture3).with("ver").and_return([
+          "\nMicrosoft Windows [Version 10.0.26200.8037]\n",
+          nil,
+          nil,
+        ])
+      end
+
+      it 'returns a resource with os.type = windows' do
+        _(detected_resource_attributes['os.type']).must_equal("windows")
+      end
+
+      it 'returns a resource with os.name = Windows' do
+        _(detected_resource_attributes['os.name']).must_equal("Windows")
+      end
+
+      it 'returns a resource with os.description (newlines deleted)' do
+        _(detected_resource_attributes['os.description']).must_equal(
+          "Microsoft Windows [Version 10.0.26200.8037]"
+        )
+      end
     end
   end
 end
